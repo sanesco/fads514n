4708b22e7be5c89f1794e3c193fa8753b66a55ea
sanesco <santiagoescobar11z@gmail.com>
2019-09-03 14:27:26 -0500

    List

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component {
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
           {/* Tu codigo va aca */}
+          {this.state.list.map((name) =>
+            <li key={name}>{name}</li>
+
+          )}
         </ul>
       </div>
     );
